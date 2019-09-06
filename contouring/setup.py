5145814146a69676a602b9a1713f08296fb79c64
Teemu Sirviö <teemu.sirvio@fmi.fi>
2019-09-06 11:11:51 +0300

    [SOL-7919] Install environment with miniconda
    We need conda and miniconda to install proj4 without problems
    in Alpine linux.

--- contouring/setup.py
+++ contouring/setup.py
@@ -1,19 +1,35 @@
 from setuptools import setup, find_packages
 
 setup(
+    # Name for PyPi (never will we upload this)
     name="contouring",
-    packages=find_packages("src"),
+    
+    # Try to search for packages here
+    packages=find_packages(where="src"),
+
+    # "Just a normal distutils thing"
+    # https://setuptools.readthedocs.io/en/latest/setuptools.html#using-find-packages
+    # This defines the "root" or something like that
     package_dir={"": "src"},
+
     # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # "scripts" keyword.
+    # Entry points provide cross-platform support and allow
     # `pip` to create the appropriate form of executable for the target
     # platform.
     #
-    # For example, the following would provide a command called `sample` which
+    # Provide a command called `contour_storm` which
     # executes the function `main` from this package when invoked:
-    entry_points={  # Optional
+    entry_points={
         'console_scripts': [
-            'sample = contouring.datareader:main',
+            'contour_storm = contouring.datareader:main',
         ],
     },
+
+    install_requires = [
+        "pytest >= 5.1",
+        "aiohttp >= 3.5",
+        "owslib >= 0.18"
+    ]
+
 )
