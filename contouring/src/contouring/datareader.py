c372e7d4f418458f64cd19f4502e24bdb78a1ae6
Teemu Sirviö <teemu.sirvio@fmi.fi>
2019-09-19 11:25:05 +0300

    Add/test generator for request times
    From given year and month generate datetimes that could be used in wfs
    requests. Generates times from 00.00 to 23.00 for each day for given
    month in one hour intervals.

--- contouring/src/contouring/datareader.py
+++ contouring/src/contouring/datareader.py
@@ -2,140 +2,49 @@
 
 from owslib.wfs import WebFeatureService
 import xml.etree.ElementTree as ET
+from datetime import datetime
+from datetime import timedelta
+from datetime import timezone
+
 
 class Datareader(object):
-    """
- 
-    Methods:
-        __init__(self, wfs_service_url, wfs_versoin)
-        print()
-    """
+    """Working between wfs-server and python"""
 
     def __init__(self, wfs_service_url='http://smartmet.fmi.fi/wfs', wfs_version='2.0.0'):
         self.service_url = wfs_service_url
         self.version = wfs_version
 
-    def _getForecastDataValidTime(self):
-        """Read latest Harmonie data begin time and end time
+    def starttimes(self, year, month):
+        """Generator for wfs-requests start times for given month.
 
-            Return:
-                tuple of starttime and endtime 
+        Starting from first day of the month at 00:00, 
+        incrementing the time with one hour a time so that next time
+        would be at 01:00. The iteration will end on last day of the 
+        month at 23:00.
+
+        Args:
+            year: Four digit string describing targeted year
+            month: Two digit string describing targeted month.
+ 
+        Yields:
+            A datetime object starting from first day of given month 
+            and ending to the last day and hour .
         """
-        storedquery_id = 'fmi::forecast::harmonie::surface::grid'
-        wfs_query = WebFeatureService(url=self.service_url, version=self.version)
-        response = wfs_query.getfeature(storedQueryID=storedquery_id, storedQueryParams={})
-        root = ET.parse(response).getroot()
-        for child in root.iter('{http://www.opengis.net/om/2.0}validTime'):
-            for i in child.iter('{http://www.opengis.net/gml/3.2}beginPosition'):
-                beginPosition = i.text
-            for i in child.iter('{http://www.opengis.net/gml/3.2}endPosition'):
-                endPosition = i.text
-        return (beginPosition, endPosition)
+        # isostring = f"{self.year}-{self.month}-01 00:00:00+0000"
+        # mytime = datetime.strptime(isostring, "%Y-%m-%d %H:%M:%S%z")
+        mytime = datetime(year, month, day=1, tzinfo=timezone.utc)
+        month = mytime.month
+        while mytime.month == month:
+            yield mytime
+            mytime = mytime + timedelta(hours=1)
 
-    def _parseWFSForecast(self, response):
-        """Format XML response to dict
-
-        Returns:
-                Dictionary with results from WFS 
-                {'timestamp': {'parameter': 'value', 'parameter': 'value'},
-                ...
-                 'timestamp': {'parameter': 'value', 'parameter': 'value'}}
-        """
-        root = ET.parse(response).getroot()
-        forecast = {}
-        for measurement_timeseries in root.iter('{http://www.opengis.net/waterml/2.0}MeasurementTimeseries'):
-            attributes = measurement_timeseries.attrib            
-            id = attributes['{http://www.opengis.net/gml/3.2}id'].split('-')[-1]
-            # Save parameter values to forecast
-            for MeasurementTVP in measurement_timeseries.iter('{http://www.opengis.net/waterml/2.0}MeasurementTVP'):
-                if MeasurementTVP[0].text not in forecast:
-                    forecast[MeasurementTVP[0].text] = {} 
-                forecast[MeasurementTVP[0].text][id] = MeasurementTVP[1].text
-        return forecast
-
-    def getWindForecast(self, latitude, longitude, height):
+    def getWFS(self, storedquery_id, stored_query_params):
         """Get latest wind speed forecast for given location
-
-            Args:
-                latitude: Target location degrees latitude
-                longitude: Target location degrees longitude
-                height: Target location height in meters (hub height)
-            Returns:
-                Dictionary with results from WFS 
-                {'timestamp': {'parameter': 'value', 'parameter': 'value'},
-                ...
-                 'timestamp': {'parameter': 'value', 'parameter': 'value'}}
         """
-        starttime, endtime = self._getForecastDataValidTime()
-        if (height < 13):
-            height = 13
-        if (height > 10000):
-            height = 10000
-        storedquery_id = 'fmi::forecast::harmonie::hybrid::point::timevaluepair'
-        parameters = ('WindSpeedMs', 'WindDirection', 'Pressure')
         wfs_query = WebFeatureService(url=self.service_url, version=self.version)
-        stored_query_params = {
-            'latlon': '{0},{1}'.format(latitude, longitude),
-            'parameters': ','.join(parameters),
-            'height': height,
-            'starttime': starttime,
-            'endtime': endtime
-        }
-        response = wfs_query.getfeature(storedQueryID=storedquery_id, storedQueryParams=stored_query_params)
-        forecast = self._parseWFSForecast(response)
-        return forecast
+        response = wfs_query.getfeature(
+            storedQueryID=storedquery_id,
+            storedQueryParams=stored_query_params
+            )
+        return response
 
-    def getRadiationGlobalFractilesForecast(self, latitude, longitude, starttime, endtime):
-        """Get forecast for F0-F100 for Global Radiation WFS
-
-            Args:
-                latitude: Target location degrees latitude
-                longitude: Target location degrees longitude
-                starttime: First available timestep in forecast
-                endtime: Last available timestep in forecast
-            Returns:
-                Dictionary with results from WFS 
-                {'timestamp': {'parameter': 'value', 'parameter': 'value'},
-                ...
-                 'timestamp': {'parameter': 'value', 'parameter': 'value'}}
-        """
-        storedquery_id = 'fmi::forecast::harmonie::surface::fractile::point::timevaluepair'
-        # parameters = ('4678', '4677', '4676', '4675', '4674', '4673', '4672')
-        parameters = ('RadiationGlobalF100', 'RadiationGlobalF90', 'RadiationGlobalF75', 'RadiationGlobalF50', 'RadiationGlobalF25', 'RadiationGlobalF10', 'RadiationGlobalF0')
-
-        wfs_query = WebFeatureService(url=self.service_url, version=self.version)
-        stored_query_params = {
-            'latlon': '{0},{1}'.format(latitude, longitude),
-            'parameters': ','.join(parameters),
-            'starttime': starttime,
-            'endtime': endtime
-        }
-        response = wfs_query.getfeature(storedQueryID=storedquery_id, storedQueryParams=stored_query_params)
-        forecast = self._parseWFSForecast(response)
-        return forecast
-
-def main():
-    dr = Datareader()
-
-    # /wfs?request=getFeature&
-    # storedquery_id=windgustcoverage
-    # starttime=2017-08-01T00:00:00Z
-    # endtime=2017-08-01T00:00:00Z
-    # source=grid
-    # bbox=21,60,24,64
-    # crs=EPSG:4326
-    # limits=10,25
-
-    latitude = 60.19206
-    longitude = 24.94583
-    height = 50000
-    response = dr.getWindForecast(latitude, longitude, height)
-    print(response)
-    latitude = 60.203561
-    longitude = 24.961179
-    starttime = "2018-09-27T05:00:00Z"
-    endtime = "2018-09-29T21:00:00Z"
-    print(dr.getRadiationGlobalFractilesForecast(latitude, longitude, starttime, endtime))
-
-if __name__ == '__main__':
-    main()
--- contouring/tests/test_datareader.py
+++ contouring/tests/test_datareader.py
@@ -1,8 +1,62 @@
 import pytest
 from contouring.datareader import Datareader
+# contents of test_app.py, a simple test for our API retrieval
+import pytest
+import os
+from datetime import timezone
+FIXTURE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+# Import the class whose method is mocked
+from owslib.wfs import WebFeatureService
 
 def test_datareader_parameters():
     dr = Datareader('localhost')
     assert dr.service_url == 'localhost'
     assert dr.version == '2.0.0'
 
+@pytest.fixture
+def windgust_multi_contours():
+    data_file = os.path.join(FIXTURE_DIR, 'windgustcoverage_contours_0-5_5-10_10-15.xml')
+    with open(data_file, 'r') as f:
+        wfs_data = f.read()
+    f.closed
+    return wfs_data
+
+# custom class to be the mock return value of WebFeatureService.getfeature()
+class MockResponse():
+    """Define methods that are required in tests"""
+    @staticmethod
+    def read():
+        return windgust_multi_contours
+
+
+# monkeypatched WebFeatureService.getfeature as fixture
+@pytest.fixture
+def mock_response(monkeypatch):
+    """WebFeatureService.getfeature() mocked to return some wfs-data."""
+
+    def mock_get(*args, **kwargs):
+        return MockResponse()
+
+    monkeypatch.setattr(WebFeatureService, "getfeature", mock_get)
+
+
+# notice our test uses the custom fixture instead of monkeypatch directly
+# def test_get_json(mock_response):
+#     dr = Datareader()
+#     result = dr.getWFS("juttu", {'juttu': 'homma'})
+#     assert result["mock_key"] == "mock_response"
+
+
+def test_starttimes():
+    dr = Datareader()
+    starttimes = dr.starttimes(2017, 8)
+    mytime = next(starttimes)
+    assert mytime.year == 2017
+    assert mytime.month == 8
+    assert mytime.hour == 0
+    assert mytime.minute == 0
+    assert mytime.tzinfo == timezone.utc
+    mytime = next(starttimes)
+    assert mytime.hour == 1
