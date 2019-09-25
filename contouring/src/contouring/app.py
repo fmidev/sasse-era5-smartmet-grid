88f7ac4446b8e41d098bc458a301f25c85cd1579
Teemu Sirviö <teemu.sirvio@fmi.fi>
2019-09-25 16:43:16 +0300

    Parse results from requests

--- contouring/src/contouring/app.py
+++ contouring/src/contouring/app.py
@@ -3,6 +3,7 @@ import contouring.datareader
 import contouring.parser
 import argparse
 from types import SimpleNamespace
+import datetime
 
 
 log = logging.getLogger(__name__)
@@ -12,76 +13,75 @@ ch = logging.StreamHandler()
 ch.setFormatter(formatter)
 log.addHandler(ch)
 
-parser = argparse.ArgumentParser(
+argparser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter
 )
 
-params = parser.add_argument_group('params', 'General settings for general stuff')
-params.add_argument("--url", help="WFS service address", default="http://smartmet.fmi.fi/wfs")
-params.add_argument("--storedquery_id", help="Name of the predefined WFS-query")
-params.add_argument("--year", help="Generate WFS-requests starting this year. Ignored if starttime is given", type=int)
-params.add_argument("--month", help="Generate WFS-requests starting this month. Ignored if starttime is given", type=int)
-params.add_argument("--verbose", help="Set logging to show all messages", action="store_true")
+params_a = argparser.add_argument_group('params', 'General settings for general stuff')
+params_a.add_argument("--url", help="WFS service address", default="http://smartmet.fmi.fi/wfs")
+params_a.add_argument("--storedquery_id", help="Name of the predefined WFS-query")
+params_a.add_argument("--year", help="Generate WFS-requests starting this year. Ignored if starttime is given", type=int)
+params_a.add_argument("--month", help="Generate WFS-requests starting this month. Ignored if starttime is given", type=int)
+params_a.add_argument("--verbose", help="Set logging to show all messages", action="store_true")
 
-params = parser.add_argument_group('Stored query settings', 'Query string fields and values.')
-params.add_argument("--limits", help="Contouring high and low values, when contouring")
-params.add_argument("--source", help="What type of data should be used by smartmet-server", choices=['grid', 'querydata'], default='grid')
-params.add_argument("--bbox", help="Bounding box coordinates for contouring area", default='21,60,24,64')
-params.add_argument("--crs", help="Coordinate system used in responses", default='EPSG:4326')
-params.add_argument("--starttime", help="Begin time for requests.")
-params.add_argument("--endtime", help="Last time interval for wfs request.")
+params_b = argparser.add_argument_group('Stored query params', 'Direct fields and values for query string.')
+params_b.add_argument("--limits", help="Contouring high and low values, when contouring")
+params_b.add_argument("--source", help="What type of data should be used by smartmet-server", choices=['grid', 'querydata'], default='grid')
+params_b.add_argument("--bbox", help="Bounding box coordinates for contouring area", default='21,60,24,64')
+params_b.add_argument("--crs", help="Coordinate system used in responses", default='EPSG:4326')
+params_b.add_argument("--starttime", help="Begin time for requests.")
+params_b.add_argument("--endtime", help="Last time interval for wfs request.")
 
-args = parser.parse_args()
+args = argparser.parse_args()
 if not args.verbose:
     log.setLevel(logging.WARN)
+
+def create_datareader():
+    datareader = contouring.datareader.Datareader(args.url)
+    datareader.stored_query_id = args.storedquery_id
+    datareader.stored_query_params.limits = args.limits
+    datareader.stored_query_params.source = args.source
+    datareader.stored_query_params.bbox = args.bbox
+    datareader.stored_query_params.crs = args.crs
+    return datareader
+
+def request_with_defined_times(datareader, starttime, endtime):
+    log.debug(f"Fetch data with given starttime {starttime} and endtime {endtime}")
+    datareader.stored_query_params.starttime = starttime
+    datareader.stored_query_params.endtime = endtime
+    return datareader.getWFS()
+
 
 
 
 def main():
-    dr = contouring.datareader.Datareader(args.url)
     
-    # Iterate over WFS-requests
+    datareader = create_datareader()
+    dataparser = contouring.parser.Parser()
+
     if args.year and args.month:
-        log.debug("Iterate stuff from generated times")
+        log.debug(f"Fetch data for {args.year}/{args.month}")
+        for starttime in contouring.datareader.starttimes(args.year, args.month):
+            formatted_starttime = starttime.strftime("%Y-%m-%dT%H:%M:%SZ")
+            results = request_with_defined_times(datareader, formatted_starttime, formatted_starttime)
+            log.debug(f"Save results to database, or something...")
+            data = results.read()
+            parsed = dataparser.list_contours_in_wfs(data)
+            log.debug(parsed)
     elif args.starttime:
-        log.debug("Fetch user defined time frame")
+        log.debug(f"Fetch data with given starttime {args.starttime}")
+        if not args.endtime:
+            endtime = args.starttime
+        results = request_with_defined_times(datareader, args.starttime, endtime)
+        log.debug(f"Save results to database, or something...")
     else:
         log.error("Not timeframe defined")
         msg = (
             "Define either --year and --month or "
             "--starttime (with optional --endtime)"
         )   
-        parser.error(msg)
-
-    # for starttime in dr.starttimes(args.year, args.month):
-
-    #     # stored_query_params can be built here
-    #     log.debug(f"{starttime}")
-    #     log.info(f"Hommaa")
-
-        ## Make request
-        # dr.getWFS()
-
-        ## Create orm-object
-
-        ## Write to data base
+        argparser.error(msg)
 
 if __name__ == '__main__':
     main()
 
-
-# import asyncio
-# import time
-# async def count():
-#     print("One")
-#     await asyncio.sleep(1)
-#     print("Two")
-# async def main():
-#     await asyncio.gather(count(), count(), count())
-# if __name__ == '__main__':
-#     # execute only if run as the entry point into the program
-#     import time
-#     s = time.perf_counter()
-#     asyncio.run(main())
-#     elapsed = time.perf_counter() - s
-#     print(f"{__file__} executed in {elapsed:0.2f} seconds.")
